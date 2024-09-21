3b24d5309441fc156c08acc0af124e30411750a1
Karthigairajan251102 <karthigairajan251102@gmail.com>
2024-09-21 16:08:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function topFunction() 
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function booked(){
+    alert("Already Booked");
+}
+function unbooked(){
+    alert("Available");
+}
